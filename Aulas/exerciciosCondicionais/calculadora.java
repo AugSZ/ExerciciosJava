85a49e35e39c100ba4d074295e1bb65a4dd8e27d
AugSZ <vitoraugusto.szslv@gmail.com>
2025-10-15 17:39:28 +0000

    conclusão exercício 3 e 4

---
+++ Aulas/exerciciosCondicionais/calculadora.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int valorA = sc.nextInt();
+        char operador = sc.next().charAt(0); //  .charAt(0) para ler um char .next() uma String
+        int valorB = sc.nextInt();
+        double resultado;
+    
+        switch (operador) {
+            case '+' -> {
+                resultado = valorA + valorB;
+            }
+            case '-' -> {
+                resultado = valorA - valorB;
+            }
+            case '*' -> {
+                resultado = valorA * valorB;
+            }
+            case '/' -> {
+                resultado = valorA / valorB;
+            }
+            default -> {
+                resultado = 0;
+                sc.close();
+                return;
+            }
+        }
+        sc.close();
+        System.out.printf("Resultado: %.2f%n", resultado);
+        
+    }
+}
--- Aulas/exerciciosCondicionais/imc.java
+++ Aulas/exerciciosCondicionais/imc.java
@@ -2,6 +2,35 @@ import java.util.Scanner;
 
 public class imc {
 public static  void main(String[] args) {
-    Scanner
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Escreva seu peso e altura");
+    double peso = sc.nextDouble();
+    double altura = sc.nextDouble();
+    
+    if (altura < 0) {                           //
+        System.out.println("Não existe");     // 
+        sc.close();
+        return;                                 //
+    }                                           //
+    if (peso < 0) {                             //
+        System.out.println("Não existe");     // acaba se for negativo
+        sc.close();
+        return;                                 //
+    }
+
+    double indiceDeMassa = peso/(altura*altura);  // Cálculo do imc
+    
+System.out.println(indiceDeMassa);
+
+    if (indiceDeMassa < 18.5) {
+        System.out.printf("Seu IMC é %.2f Abaixo do peso. Por favor se cuide", indiceDeMassa);
+    }
+    else if (indiceDeMassa >= 18.5 && indiceDeMassa <= 24.9) {
+        System.out.printf("Seu IMC é %.2f Peso ok (normal não é um bom termo)", indiceDeMassa);
+    }
+
+    else if (indiceDeMassa >= 24.5) {
+        System.out.printf("Seu IMC é %.2f, Obesidade. Por favor se cuide", indiceDeMassa);
+    }
     }
 }
